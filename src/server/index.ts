6cb9c15155350962eaef6658619717b7854509f2
Levezze <lev.zhi@gmail.com>
2025-04-15 17:36:47 -0400

    add '--loader ts-node/esm' to package.json to run ts file server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { ExpressAuth } from '@auth/express';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
+});
